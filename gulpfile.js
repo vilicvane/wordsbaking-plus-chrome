2328225dc560ae2249490e036af6093a3edb1979
vilicvane <i@vilic.info>
2017-05-14 00:13:36 +0800

    Bump version

--- gulpfile.js
+++ gulpfile.js
@@ -22,22 +22,22 @@ Gulp.task('bump', function () {
     var versionRegex = /("version"\s*:\s*)"([^"]+)"/;
 
     var manifestJson = FS.readFileSync(manifestFilePath, 'utf-8');
-    
+
     manifestJson = manifestJson.replace(versionRegex, function (text, prefix, version) {
         return prefix + '"' + SemVer.inc(version, 'patch') + '"';
     });
-    
+
     FS.writeFileSync(manifestFilePath, manifestJson);
 });
 
 Gulp.task('pack', function () {
     var archive = Archiver.create('zip', {});
-    
+
     filesToPackGlobs.forEach(function (glob) {
         archive.glob(glob);
     });
-    
+
     archive.finalize();
-    
+
     return archive.pipe(FS.createWriteStream(zipPackageName));
 });
